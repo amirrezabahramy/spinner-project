7cc27fec907ad027299ed61e8ec46a16d365d5c7
amirrezabahramy <amirrezabahramy@gmail.com>
2024-02-20 06:22:24 +0330

    Added styles for disabled mode

--- src/components/basic/Button.jsx
+++ src/components/basic/Button.jsx
@@ -6,9 +6,9 @@ function Button({ className = "", variant = "solid", ...rest }) {
       className={twMerge(
         "py-2 px-3 rounded transition",
         variant === "solid" &&
-          "bg-blue-500 text-white hover:bg-blue-600 active:bg-blue-700",
+          "bg-blue-500 text-white hover:bg-blue-600 active:bg-blue-700 disabled:bg-blue-200",
         variant === "outline" &&
-          "bg-transparent border border-blue-500 text-blue-500 hover:bg-blue-100",
+          "bg-transparent border border-blue-500 text-blue-500 hover:bg-blue-100 disabled:border-blue-200 disabled:text-blue-200 hover:disabled:bg-transparent",
         className
       )}
       {...rest}
