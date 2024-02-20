36d536b09560fb896c02eb8b27d532124279f378
amirrezabahramy <amirrezabahramy@gmail.com>
2024-02-20 06:11:46 +0330

    Added Navigation icon

---
+++ src/components/icons/Navigation.jsx
@@ -0,0 +1,22 @@
+function Navigation(props) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      xmlnsXlink="http://www.w3.org/1999/xlink"
+      version="1.1"
+      x="0px"
+      y="0px"
+      viewBox="0 0 48 60"
+      enableBackground="new 0 0 48 48"
+      xmlSpace="preserve"
+      {...props}
+    >
+      <path
+        fill="#000000"
+        d="M24,0.5c-9.1,0-16.6,7.4-16.6,16.6c0,8.8,15.2,28.6,15.8,29.4l0.8,1l0.8-1c0.6-0.8,15.8-20.6,15.8-29.4  C40.6,7.9,33.1,0.5,24,0.5z M24,10.2c3.8,0,6.9,3.1,6.9,6.9c0,3.8-3.1,6.9-6.9,6.9s-6.9-3.1-6.9-6.9C17.1,13.3,20.2,10.2,24,10.2z"
+      />
+    </svg>
+  );
+}
+
+export default Navigation;
